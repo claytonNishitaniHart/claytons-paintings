6270dbe85bdd76e0642a7b9c6e37c31cbcd35585
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-08-05 22:12:11 +1200

    added button to scroll to paintings

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import './App.scss';
 import photo1 from './paintings/photo1.jpg';
 
 function App() {
+  function takeALook() {
+    let element = document.getElementById('gallery');
+    element.scrollIntoView({behavior: "smooth"});
+  }
+  
   return (
     <div>
       <div className={'App'}>
       </div>
       <h1 className={'Heading'}>PAINT</h1>
       <h2 className={'SubHeading'}>trying to become bob ross</h2>
-      <div className={'Gallery'}>
+      <div className={'View-container'}>
+        <button className={'View-button'} onClick={takeALook}/>
+      </div>
+      <div id={'gallery'} className={'Gallery'}>
         <div className={'Gallery-item'}>
           <img src={photo1} className={'photo'} alt={'1'}/>
           <p className={'Gallery-item-text'}>Island in the Wilderness</p>
